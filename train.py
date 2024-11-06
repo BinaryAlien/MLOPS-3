75f580812434e125cc6525d2bea1def1e4d4caf0
Binary <49949198+BinaryAlien@users.noreply.github.com>
2024-11-06 14:44:55 +0100

    Part 1

---
+++ train.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+from mlflow.models import infer_signature
+from sklearn import datasets
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+import mlflow
+
+MLFLOW_HOST = "127.0.0.1"
+MLFLOW_PORT = 8080
+
+mlflow.set_tracking_uri(uri=f"http://{MLFLOW_HOST}:{MLFLOW_PORT}")
+
+# Load the Iris dataset
+X, y = datasets.load_iris(return_X_y=True)
+
+# Split the data into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+# Define the model hyperparameters
+params = {
+    "solver": "lbfgs",
+    "max_iter": 1000,
+    "random_state": 8888,
+}
+
+# Train the model
+model = LogisticRegression(**params)
+model.fit(X_train, y_train)
+
+# Predict on the test set
+y_pred = model.predict(X_test)
+
+# Calculate metrics
+accuracy = accuracy_score(y_test, y_pred)
+
+# Create a new MLflow Experiment
+mlflow.set_experiment("Iris Model")
+
+# Start an MLflow run
+with mlflow.start_run():
+    # Log the hyperparameters
+    mlflow.log_params(params)
+
+    # Log the loss metric
+    mlflow.log_metric("accuracy", accuracy)
+
+    # Infer the model signature
+    signature = infer_signature(X_train, model.predict(X_train))
+
+    # Log the model
+    model_info = mlflow.sklearn.log_model(
+        sk_model=model,
+        artifact_path="iris_model",
+        signature=signature,
+        input_example=X_train,
+        registered_model_name="iris-model",
+    )
